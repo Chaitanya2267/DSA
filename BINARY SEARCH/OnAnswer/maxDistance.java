138b078422c6305f04f6da86e4e535f3a8fcf83f
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-08-14 21:32:28 +0530

    Implement maxDistance method for positioning

---
+++ BINARY SEARCH/OnAnswer/maxDistance.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int low = 1;
+        int high = position[position.length - 1] - position[0];
+        int ans  = 0;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(canPlace(position, m, mid)) {
+                ans = mid;
+                low = mid + 1;
+            } else {high = mid -1;}
+        }
+        return ans;
+    }
+    public boolean canPlace(int[] position, int m, int d){
+        int count = 1;
+        int lastPos = position[0];
+        for(int i = 1; i < position.length; i++) {
+            if(position[i] - lastPos >= d) {
+                count++;
+                lastPos = position[i];
+            }
+            if(count >= m) return true;
+        }
+        return false;
+    }
+}
