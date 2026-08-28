9f3421143733366a5cf0fba278437d8aa9fda1c7
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-08-28 08:59:17 +0530

    Implement oddEvenList to rearrange nodes
    Refactor oddEvenList method to separate odd and even nodes into two lists and then combine them.

--- LINKED_LIST/oddEvenList.java
+++ LINKED_LIST/oddEvenList.java
@@ -20,3 +20,53 @@ class Solution {
         return head;
     }
 }
+// ---------------------------------------------------------------------------------------------
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode evenHead = null, evenTail = null;
+        ListNode oddHead = null, oddTail = null;
+
+        ListNode current = head;
+
+        int position = 1;
+
+        while (current != null) {
+
+            ListNode nextNode = current.next;
+            current.next = null;
+
+            if (position % 2 == 0) {
+
+                if (evenHead == null) {
+                    evenHead = current;
+                    evenTail = current;
+                } else {
+                    evenTail.next = current;
+                    evenTail = current;
+                }
+
+            } else {
+
+                if (oddHead == null) {
+                    oddHead = current;
+                    oddTail = current;
+                } else {
+                    oddTail.next = current;
+                    oddTail = current;
+                }
+            }
+
+            current = nextNode;
+            position++;
+        }
+
+        if (oddTail != null) {
+            oddTail.next = evenHead;
+        }
+
+        return oddHead;
+    }
+}
