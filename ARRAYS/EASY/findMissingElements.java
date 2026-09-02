7b3773327aa06a9efebfbc1d93a351c1f9dfd4e4
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-09-02 21:12:07 +0530

    Implement method to find missing elements in array

---
+++ ARRAYS/EASY/findMissingElements.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        boolean[] present = new boolean[max - min + 1];
+
+        for (int num : nums) {
+            present[num - min] = true;
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < present.length; i++) {
+            if (!present[i]) {
+                result.add(i + min);
+            }
+        }
+
+        return result;
+    }
+}
