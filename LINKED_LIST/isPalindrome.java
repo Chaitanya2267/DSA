1f84daa8dff36d1ac8a3cd63df1a454a6e32739d
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-09-03 22:07:51 +0530

    Implement palindrome check for linked list

--- LINKED_LIST/isPalindrome.java
+++ LINKED_LIST/isPalindrome.java
@@ -24,3 +24,54 @@ class Solution {
         return true;
     }
 }
+// ---------------------------------------------------------------
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverseLinkedList(slow.next);
+
+        ListNode first = head;
+        ListNode second = secondHalf;
+        boolean palindrome = true;
+
+        while (second != null) {
+            if (first.val != second.val) {
+                palindrome = false;
+                break;
+            }
+
+            first = first.next;
+            second = second.next;
+        }
+
+        slow.next = reverseLinkedList(secondHalf);
+
+        return palindrome;
+    }
+
+    private ListNode reverseLinkedList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+}
