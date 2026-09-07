c6cc3a43fe639f1fa86dbab16adb8c329984e74b
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-09-07 09:06:23 +0530

    Implement removeElements to delete nodes by value
    203. Remove Linked List Elements

---
+++ LINKED_LIST/removeElements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode res = dummy;
+
+        while (dummy.next != null) {
+            if (dummy.next.val == val) {
+                dummy.next = dummy.next.next;
+            } else {
+                dummy = dummy.next;
+            }
+        }
+        return res.next;
+    }
+}
