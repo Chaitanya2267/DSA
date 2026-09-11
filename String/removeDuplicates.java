c1d95621d0b83c6410d87d5be361b2615fc1c7a2
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-09-11 21:06:53 +0530

    Add initial implementation for removeDuplicates method of string

---
+++ String/removeDuplicates.java
@@ -0,0 +1,19 @@
+// 1047. Remove All Adjacent Duplicates In String
+
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (stack.isEmpty() || stack.peek() != ch) {
+                stack.push(ch);
+            } else {
+                stack.pop();
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        for (char ch : stack) {
+            ans.append(ch);
+        }
+        return ans.toString();
+    }
+}
