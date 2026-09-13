8498dc8be1add4dbecc071b35bca06d463732e81
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-09-13 19:04:20 +0530

    implemented 61. Rotate List
    Given the head of a linked list, rotate the list to the right by k places.

---
+++ LINKED_LIST/rotateRight.java
@@ -0,0 +1,25 @@
+// 61. Rotate List
+
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+        tail.next = head;
+        k = k % length;
+        int stepToNewTail = length - k;
+        ListNode newTail = head;
+        for (int i = 1; i < stepToNewTail; i++) {
+            newTail = newTail.next;
+        }
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+        return newHead;
+    }
+}
