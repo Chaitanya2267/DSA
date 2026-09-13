c6e56e5d93a276e2c09e8744b9918837ae5bf260
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-09-13 19:14:29 +0530

    implemented 25. Reverse Nodes in k-Group
    Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.

---
+++ LINKED_LIST/reverseKGroup.java
@@ -0,0 +1,38 @@
+// 25. Reverse Nodes in k-Group
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode groupPrev = dummy;
+        while (true) {
+            ListNode kth = getkthNode(groupPrev, k);
+            if (kth == null)
+                break;
+
+            ListNode groupNext = kth.next;
+
+            ListNode prev = groupNext;
+            ListNode curr = groupPrev.next;
+
+            for (int i = 0; i < k; i++) {
+                ListNode temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+            ListNode temp = groupPrev.next;
+            groupPrev.next = kth;
+            groupPrev = temp;
+        }
+        return dummy.next;
+    }
+
+    private ListNode getkthNode(ListNode curr, int k) {
+        while (curr != null && k > 0) {
+            curr = curr.next;
+            k--;
+        }
+        return curr;
+    }
+}
