ffac72ee147bc7a1855ba1b34e9feb1ff8259e64
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-09-16 20:39:09 +0530

    Update mergeKLists.java
    A better approach is to merge the two lists

--- LINKED_LIST/mergeKLists.java
+++ LINKED_LIST/mergeKLists.java
@@ -26,3 +26,43 @@ class Solution {
         return dummy.next;
     }
 }
+// -----------------------------------------------------------
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        ListNode head = lists[0];
+
+        for (int i = 1; i < lists.length; i++) {
+            head = merge2Lists(head, lists[i]);
+        }
+
+        return head;
+    }
+
+    private ListNode merge2Lists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        curr.next = (l1 != null) ? l1 : l2;
+
+        return dummy.next;
+    }
+}
+// -----------------------------------------------------------
+
