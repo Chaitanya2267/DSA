1753150327dea39a042c6b759a06d61c5fc79d20
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-09-23 18:33:57 +0530

    Create twoSum2.java
    O(nlogn) complexity implemented.

---
+++ BINARY SEARCH/On1D_array/twoSum2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int complement = target - numbers[i];
+
+            // Binary search in the range [i + 1, n - 1]
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (numbers[mid] == complement) {
+                    // 1-based indexing required by LeetCode 167
+                    return new int[] { i + 1, mid + 1 };
+                } else if (numbers[mid] < complement) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
